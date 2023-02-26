06abc7e38cb6705da2299ce3bb7473a3f06970c7
kamil_vps <root@vmi1178424.contaboserver.net>
2023-02-26 19:07:42 +0100

    cena > 0 i cors

--- ceny.php
+++ ceny.php
@@ -6,7 +6,10 @@ function cors()
   if (isset($_SERVER['HTTP_ORIGIN'])) {
     // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
     // you want to allow, and if so:
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+      
+    header("Access-Control-Allow-Origin: *");
+      
     header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Max-Age: 86400');    // cache for 1 day
   }
@@ -292,7 +295,7 @@ if (!prawidloweTLD($tld)) {
     #
     $cennik_wal = array();
     for ($i = 0; $i < count($cennik); $i++) {
-      if ($cennik[$i] != null) {
+      if ($cennik[$i] != null && $cennik[$i]>0) {
         $cennik_wal[] = $cennik[$i];
       }
     }
